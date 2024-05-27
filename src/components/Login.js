2b7cd67a05ff1b58893ed7c0bcb602783891e12d
Akash Singh <2081993akash@gmail.com>
2024-05-27 09:24:43 +0530

    Sign In Integrated

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,10 @@
 import { useRef, useState } from "react";
 import Header from "./Header";
 import { checkValidData } from "../utils/validate";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import { auth } from "../utils/firebase";
 
 const Login = () => {
@@ -43,10 +46,26 @@ const Login = () => {
         .catch((error) => {
           const errorCode = error.code;
           const errorMessage = error.message;
-          setErrorMessage(error.code + ": " + errorMessage);
+          setErrorMessage(errorCode + ": " + errorMessage);
           // ..
         });
     } else if (isSignInForm) {
+      signInWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          console.log(user);
+          // ...
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setErrorMessage(errorCode + ": " + errorMessage);
+        });
     }
   };
 
