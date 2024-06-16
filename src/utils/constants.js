c82501ab37fbc88e404fffc123e2a80c5bb9ebd6
Akash Singh <2081993akash@gmail.com>
2024-06-16 11:13:54 +0530

    Key Issue Fix

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -6,7 +6,7 @@ export const API_OPTIONS = {
   method: "GET",
   headers: {
     accept: "application/json",
-    Authorization: "Bearer " + process.env.REACT_APP_TMDB_KEY,
+    Authorization: `Bearer ${process.env.REACT_APP_TMDB_KEY}`,
   },
 };
 export const MOVIE_POSTER_URL = "https://image.tmdb.org/t/p/w780";
